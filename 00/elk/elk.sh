23bd3f5d9f49edb866ca9bda34647b9f4d3ce33e
康小征 <2472843746@qq.com>
2021-03-16 10:41:39 +0800

    refactor: elk升级本版
    7.11.2

---
+++ 00/elk/elk.sh
@@ -0,0 +1,44 @@
+docker login --username=13552536075 registry.cn-hangzhou.aliyuncs.com
+docker pull registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:logstash_7.11.2
+docker tag registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:logstash_7.11.2 docker.elastic
+.co/logstash/logstash:7.11.2
+docker rmi registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:logstash_7.11.2
+
+docker pull registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:kibana_7.11.2
+docker tag registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:kibana_7.11.2 docker.elastic.co/kibana/kibana:7.11.2
+docker rmi registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:kibana_7.11.2
+
+docker pull registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:elasticsearch_7.11.2
+docker tag registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:elasticsearch_7.11.2 docker.elastic.co/elasticsearch/elasticsearch:7.11.2
+docker rmi registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:elasticsearch_7.11.2
+
+docker pull registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:auditbeat_7.11.2
+docker tag registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:auditbeat_7.11.2 docker.elastic.co/beats/auditbeat:7.11.2
+docker rmi registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:auditbeat_7.11.2
+
+docker pull registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:filebeat_7.11.2
+docker tag registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:filebeat_7.11.2 docker.elastic.co/beats/filebeat:7.11.2
+docker rmi registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:filebeat_7.11.2
+
+
+docker pull registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:heartbeat_7.11.2
+docker tag registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:heartbeat_7.11.2 docker.elastic.co/beats/heartbeat:7.11.1
+docker rmi registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:heartbeat_7.11.2
+
+docker pull registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:metricbeat_7.11.2
+docker tag registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:metricbeat_7.11.2 docker.elastic.co/beats/metricbeat:7.11.2
+docker rmi registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:metricbeat_7.11.2
+
+docker pull registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:packetbeat_7.11.2
+docker tag registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:packetbeat_7.11.2 docker.elastic.co/beats/packetbeat:7.11.2
+docker rmi registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:packetbeat_7.11.2
+
+
+
+
+
+
+
+docker pull registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:
+docker pull registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:
+docker pull registry.cn-hangzhou.aliyuncs.com/kang_uat/k8s_1:
